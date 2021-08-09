ebcb715c61b1c0b2ab92dc4cb3f1fd0366363ef3
thefirstblair <thanyatorn.kr@ku.th>
2021-08-09 13:06:06 +0700

    add trade controller

--- api/reward/controllers/reward.js
+++ api/reward/controllers/reward.js
@@ -5,4 +5,19 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async trade(ctx) {
+        const { id } = ctx.params;
+
+        const entity = await strapi.services.reward.findOne({ id }); // find a item point
+        ctx.state.user.score = ctx.state.user.score - entity.points;
+
+        strapi.query('user', 'users-permissions').update(
+            { id: ctx.state.user.id },ctx.state.user
+        )
+
+        return {
+            response:200
+        };
+    }
+};
